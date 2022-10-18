8ed493f6f6cc924902392d3b320302ce2766b3e7
ersinev <ersinevci@hotmail.com>
2022-10-18 17:03:10 +0200

    start of advanced serach

--- src/AdvancedSearch.js
+++ src/AdvancedSearch.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Container, Col, Row,InputGroup,Form} from'react-bootstrap'
+import {FcSearch} from 'react-icons/fc'
 
 function AdvancedSearch() {
   return (
-    <div>AdvancedSearch</div>
+    <>
+    <Container>
+      <Row>
+        <Col>
+              <InputGroup className="mb-3">
+              
+              <Form.Control
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+              <InputGroup.Text id="basic-addon1"><FcSearch></FcSearch></InputGroup.Text>
+              </InputGroup>
+        </Col>
+      </Row>
+    </Container>
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
   )
 }
 
