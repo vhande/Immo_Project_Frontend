2e4dd0d0ed5fc3c2e3004fc6f32c2e84f12feb39
ersinev <ersinevci@hotmail.com>
2022-10-20 13:23:25 +0200

    updated

--- src/Ad.js
+++ src/Ad.js
@@ -5,36 +5,26 @@ function Ad() {
   return (
     <>
       <Container className="justify-content-center text-center adContainer">
-        <Row>
-          <Col md={12}>
-            <h2>Publication of your classified</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p>
-              We recommend you to provide as many details as possible to
-              optimize the quality of your classified.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Nav
-              variant="pills"
-              defaultActiveKey="#"
-              className="m-1 pills d-flex justify-content-center border rounded align-items-center"
-            >
-              <Nav.Item>
-                <Nav.Link eventKey="link-1">Sell</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="link-2">Rent</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-        </Row>
-        
+        <h2>Publication of your classified</h2>
+
+        <p>
+          We recommend you to provide as many details as possible to optimize
+          the quality of your classified.
+        </p>
+        <div className="d-flex justify-content-center align-items-center">
+          <Nav
+            variant="pills"
+            defaultActiveKey="#"
+            className="m-1 pills center-pills justify-content-center border rounded "
+          >
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Sell</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Rent</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
       </Container>
     </>
   );
