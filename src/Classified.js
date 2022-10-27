0fafa1c64efb6f371fd5ff847c2c419c5a4ed757
ersinev <ersinevci@hotmail.com>
2022-10-27 09:45:12 +0200

    Classified Album

--- src/Classified.js
+++ src/Classified.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import ClassifiedAlbum from "./ClassifiedAlbum";
 
 function Classified() {
   const { id } = useParams();
@@ -17,9 +16,11 @@ function Classified() {
               <h2>House for sale</h2>
               <p>4 bedrooms | 275m2</p>
               <p>Adress</p>
+              
             </Col>
-            <Col md={3}>
+            <Col md={3} className="d-column pt-3 classifiedImages">
               <h2>565$</h2>
+             
             </Col>
             <Col md={3}>
               <p>House code</p>
@@ -29,28 +30,34 @@ function Classified() {
       </Container>
       <Container>
         <Row>
-          <Col md={4}>
-            <ClassifiedAlbum />
-          </Col>
-          <Col md={4} className="d-column pt-3">
+          <Col md={6} sm={6} >
+           
             <img
-              className="classifiedPhoto"
-              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-              alt="new"
-            />
-            <img
-              className="classifiedPhoto"
-              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-              alt="new"
-            />
-            <img
-              className="classifiedPhoto"
-              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+              className="classifiedPhotoBig"
+              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=450"
               alt="new"
             />
           </Col>
+          <Col md={3} sm={0} className="d-column classifiedImages">
+            
+            <img
+              className="classifiedPhoto"
+              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=450"
+              alt="new"
+            />
+            <img
+              className="classifiedPhoto"
+              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=450"
+              alt="new"
+            />
+            <img
+              className="classifiedPhoto"
+              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=450"
+              alt="new"
+            />
+          </Col>
+          
         </Row>
-        
       </Container>
     </>
   );
