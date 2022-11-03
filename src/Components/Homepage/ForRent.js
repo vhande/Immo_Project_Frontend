faf39a4355972f5dba6b26e29e8f2243959e7daa
ersinev <ersinevci@hotmail.com>
2022-11-03 11:20:29 +0100

    most popular part updated

--- src/Components/Homepage/ForRent.js
+++ src/Components/Homepage/ForRent.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Container, Card, Button} from 'react-bootstrap'
-import {GrNext} from 'react-icons/gr'
+import {FcNext} from 'react-icons/fc'
 
 function ForRent() {
   return (
     <Container fluid>
     <h3 className="search-container p-3" style={{"color":"var(--blue)"}}>Most Popular Properties</h3>
-    <Container className="mb-3 d-flex flex-column justify-content-between align-items-center flex-md-row flex-lg-row flex-xl-row">
+    <Container className="mb-3 d-flex flex-column justify-content-between  flex-md-row flex-lg-row flex-xl-row">
     <Card className="m-2" style={{"width":"22rem"}}>
       <Card.Img style={{"max-height":"200px"}} variant="top" src="https://picsum.photos/200" />
       <Card.Body>
@@ -27,9 +27,9 @@ function ForRent() {
     </Card>
     <Card className="m-2"style={{"width":"22rem"}}>
 
-      <Card.Body className="d-flex  flex-column align-items-center justify-content-center">
+      <Card.Body className="d-flex  flex-column align-items-center  justify-content-center">
           <Card.Title>See All Popular Properties</Card.Title>
-          <Button><GrNext fill="#FFFF" /></Button>
+          <Button style={{width:'50%', height:"20%", marginTop:'10px'}}><FcNext/></Button>
       </Card.Body>
     </Card>
     </Container>
