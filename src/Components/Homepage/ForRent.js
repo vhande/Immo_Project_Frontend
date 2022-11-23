c2c1011fbdc698d8e88e573df498e2d0aeec90e4
Hande <hande@Handes-MacBook-Air.local>
2022-11-23 14:25:48 +0100

    boxsizeissuefixed

--- src/Components/Homepage/ForRent.js
+++ src/Components/Homepage/ForRent.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {Container, Card, Button} from 'react-bootstrap'
 import {FcNext} from 'react-icons/fc'
 import {useEffect, useState} from 'react'
+import useMeasure from "react-use-measure";
 
 function ForRent() {
   const [result, setResult] = useState([])
+  const [ref, {height}] = useMeasure()
 
   useEffect(() => {
     const action = () => {
@@ -22,24 +24,24 @@ function ForRent() {
   }
   return (
     <Container fluid>
-    <h3 className="search-container p-3" style={{"color":"var(--blue)"}}>Most Recent Properties</h3>
-    <Container className="mb-3 d-flex flex-column justify-content-between justify-content-sm-center align-items-center flex-md-row flex-lg-row flex-xl-row">
+    <h3 className="p-3" style={{"color":"var(--blue)"}}>Most Recent Properties</h3>
+    <Container fluid className="mb-3 d-flex flex-column flex-md-row flex-lg-row flex-xl-row  align-items-center">
     {result.length !== 0 ? 
     result.slice(0,2).map(item=> 
-    <Card className="m-2">
-      <Card.Img className="card-img for-rent" variant="top" src={`https://immo-backend.herokuapp.com${item.file}`} />
+    <Card ref={ref} className="m-2" style={{width: "100%",height: "auto"}}>
+      <Card.Img className="card-img" variant="top" src={`https://immo-backend.herokuapp.com${item.file}`} />
       <Card.Body>
-        <Card.Title style={{"fontSize":"0.9em"}}>{toUpperCase(item.type)}</Card.Title>
-        <Card.Title className="my-1" style= {{"fontSize":"1.4em"}}>€{item.price}</Card.Title>
-        <Card.Title className="m-0 text-secondary"style={{"fontSize":"0.9em"}}>{item.bedrooms} bdr.</Card.Title>
-        <Card.Title className="m-0 text-secondary" style={{"fontSize":"0.9em"}}>{toUpperCase(item.city)}</Card.Title>
+        <Card.Title className="text-break" style={{"fontSize":"0.9em"}}>{toUpperCase(item.type)}</Card.Title>
+        <Card.Title className="my-1 text-break" style= {{"fontSize":"1.4em"}}>€{item.price}</Card.Title>
+        <Card.Title className="m-0 text-secondary text-break"style={{"fontSize":"0.9em"}}>{item.bedrooms} bdr.</Card.Title>
+        <Card.Title className="m-0 text-secondary text-break" style={{"fontSize":"0.9em"}}>{toUpperCase(item.city)}</Card.Title>
       </Card.Body>
     </Card> )
     : "" }
-    <Card className="m-2 for-rent">
+    <Card className="m-2" style={{width: "100%",height: height}} >
       <Card.Body className="d-flex flex-column align-items-center justify-content-center">
-          <Card.Title>See All Recent Properties</Card.Title>
-          <Button href="/search/sale/house" style={{width:'50%', height:"20%", marginTop:'10px'}}><FcNext style={{fontSize: '40px'}}/></Button>
+          <Card.Title className="text-center pb-2">See All Recent Properties</Card.Title>
+          <Button className="d-flex align-items-center justify-content-center" href="/search/sale/house" style={{width:'50%', height:"20%"}}><FcNext size="40px" color="white"/></Button>
       </Card.Body>
     </Card>
     </Container>
